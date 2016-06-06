8b2515d5aa33b26fa38016582ebc637ee396c970
Isawan Millican <admin@isawan.net>
2016-06-06 21:11:25 +0100

    Changed || to or. Forgot I was using python for a moment there

--- battleship.py
+++ battleship.py
@@ -6,18 +6,17 @@ def lettotup(pos):
     let = dict(zip(string.ascii_lowercase,[ord(c)%32 for c in string.ascii_lowercase]))
     return (int(pos[0])-1,let[pos[1]]-1)
 
+# TODO: Test
 def test_ship(board,ship):
     def is_out_of_bounds():
         def outbounds(x,y,sx,sy):
-            return x < 0 || y < 0 || x => sx || y =>y
-        return (outbounds(ship[0],ship[1],board.shape[0],board.shape[1]) ||
+            return x < 0 or y < 0 or x => sx or y =>y
+        return (outbounds(ship[0],ship[1],board.shape[0],board.shape[1]) or
                 outbounds(ship[0] + ship[2]-1, ship[1] + ship[3] -1,
                     board.shape[0],board.shape[1]))
     def is_legal():
         for jj in xrange(ship[1], ship[3]):
             for ii in xrange(ship[0], ship[2]):
-                if board[jj][ii] == 'x' ||  board [jj][ii] == 'n':
+                if board[jj][ii] == 'x' or  board [jj][ii] == 'n':
                     return false
     return not ( is_out_of_bounds() && is_legal() )
-
-
