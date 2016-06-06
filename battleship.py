2ce9a1d2c13d80ae47c6a57347424c89d9b26794
Isawan Millican <admin@isawan.net>
2016-06-06 20:55:09 +0100

    Added text coordinate to turple coordinate function Added legal move test

---
+++ battleship.py
@@ -0,0 +1,23 @@
+import string
+import numpy as np
+import matplotlib.pyplot as plt
+
+def lettotup(pos):
+    let = dict(zip(string.ascii_lowercase,[ord(c)%32 for c in string.ascii_lowercase]))
+    return (int(pos[0])-1,let[pos[1]]-1)
+
+def test_ship(board,ship):
+    def is_out_of_bounds():
+        def outbounds(x,y,sx,sy):
+            return x < 0 || y < 0 || x => sx || y =>y
+        return (outbounds(ship[0],ship[1],board.shape[0],board.shape[1]) ||
+                outbounds(ship[0] + ship[2]-1, ship[1] + ship[3] -1,
+                    board.shape[0],board.shape[1]))
+    def is_legal():
+        for jj in xrange(ship[1], ship[3]):
+            for ii in xrange(ship[0], ship[2]):
+                if board[jj][ii] == 'x' ||  board [jj][ii] == 'n':
+                    return false
+    return not ( is_out_of_bounds() && is_legal() )
+
+
