220004f9197fa0320474623615d7079c7b91235d
Isawan Millican <admin@isawan.net>
2016-06-09 22:57:00 +0100

    Added function for generating grid of possible way of arranging ship

--- battleship.py
+++ battleship.py
@@ -2,21 +2,52 @@ import string
 import numpy as np
 import matplotlib.pyplot as plt
 
+WTR = 0
+SHP = 1
+CLR = 2
+
+bombable = set([WTR])
+
 def lettotup(pos):
     let = dict(zip(string.ascii_lowercase,[ord(c)%32 for c in string.ascii_lowercase]))
     return (int(pos[0])-1,let[pos[1]]-1)
 
-# TODO: Test
-def test_ship(board,ship):
+def valid_placement(board,ship):
     def is_out_of_bounds():
         def outbounds(x,y,sx,sy):
-            return x < 0 or y < 0 or x => sx or y =>y
-        return (outbounds(ship[0],ship[1],board.shape[0],board.shape[1]) or
+            return x < 0 or y < 0 or x >= sx or y >= sy
+        return (outbounds(ship[0],ship[1],board.shape[1],board.shape[0]) or
                 outbounds(ship[0] + ship[2]-1, ship[1] + ship[3] -1,
-                    board.shape[0],board.shape[1]))
+                    board.shape[1],board.shape[0]))
     def is_legal():
-        for jj in xrange(ship[1], ship[3]):
-            for ii in xrange(ship[0], ship[2]):
-                if board[jj][ii] == 'x' or  board [jj][ii] == 'n':
-                    return false
-    return not ( is_out_of_bounds() and is_legal() )
+        for jj in range(ship[1], ship[1] + ship[3]):
+            for ii in range(ship[0], ship[0] + ship[2]):
+                if board[jj][ii] == CLR:
+                    return False
+        return True
+    return ( not is_out_of_bounds() ) and is_legal()
+
+def ship_combinations(x,y,shiplength):
+    ships = []
+    for ii in range(shiplength):
+        #horizontal
+        ships.append((x-ii,y,shiplength,1))
+        #vertical
+        ships.append((x,y-ii,1,shiplength))
+    return set(ships)
+
+def valid_combinations(board,x,y,shiplength):
+    combo = []
+    for ship in ship_combinations(x,y,shiplength):
+        if valid_placement(board,ship):
+            combo.append(ship)
+    return set(combo)
+
+def combo_grid(board,shiplengths):
+    combogrid = np.zeros(board.shape)
+    for jj in range(board.shape[0]):
+        for ii in range(board.shape[1]):
+            for ss in shiplengths:
+                combogrid[jj][ii] += len(valid_combinations(board,ii,jj,ss))
+    return combogrid
+
--- test.py
+++ test.py
@@ -59,4 +59,48 @@ def test_valid_placement():
 
     print('Valid placement test passed')
 
+def test_combinations():
+    board = np.array([
+            [WTR, WTR, WTR, WTR, WTR, WTR],
+            [WTR, WTR, WTR, WTR, WTR, WTR],
+            [WTR, CLR, WTR, CLR, WTR, WTR],
+            [WTR, WTR, SHP, WTR, WTR, WTR],
+            [WTR, CLR, WTR, CLR, WTR, WTR],
+            [WTR, WTR, WTR, WTR, WTR, WTR]
+            ])
+
+    expected_ship_result = {
+            (0,0,1):set([(0,0,1,1)]),
+            (0,0,3):set([(0,0,3,1),(0,0,1,3)]),
+            (1,0,3):set([(0,0,3,1),(1,0,3,1)]),
+            }
+    
+    for ii,jj in expected_ship_result.items():
+        assert(btl.valid_combinations(board,*ii) == jj)
+    print('Combination test passed')
+
+def test_combo_grid():
+    board = np.array([
+        [WTR, WTR, WTR, WTR, WTR, WTR],
+        [WTR, WTR, WTR, WTR, WTR, WTR],
+        [WTR, WTR, CLR, WTR, CLR, WTR],
+        [WTR, WTR, WTR, SHP, WTR, WTR],
+        [WTR, WTR, CLR, WTR, CLR, WTR],
+        [WTR, WTR, WTR, WTR, WTR, WTR],
+        [WTR, WTR, WTR, WTR, WTR, WTR],
+        [WTR, WTR, WTR, WTR, WTR, WTR]
+        ])
+    expected_combo = np.array([[6., 9., 9., 11., 7., 6.],
+            [  9.,  12.,   9.,  14.,   7.,   9.],
+            [  9.,   9.,   0.,   8.,   0.,   8.],
+            [ 12.,  15.,   8.,  17.,   6.,  12.],
+            [ 10.,  10.,   0.,   9.,   0.,   9.],
+            [ 11.,  14.,  10.,  16.,   8.,  11.],
+            [  9.,  12.,  11.,  14.,   9.,   9.],
+            [  6.,   9.,  10.,  11.,   8.,   6.]])
+
+    assert(np.array_equal(btl.combo_grid(board,set([2,3,4])), expected_combo))
+
 test_valid_placement()
+test_combinations()
+test_combo_grid()
